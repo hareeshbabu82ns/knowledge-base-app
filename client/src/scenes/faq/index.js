cd396aedd49c101eadf0d40cea4264c0b6354576
Hareesh Polla <hareeshbabu82ns@gmail.com>
2023-03-03 15:45:19 -0700

    profile, calander, faq pages

--- client/src/scenes/faq/index.js
+++ client/src/scenes/faq/index.js
@@ -1,7 +1,45 @@
+import { Box, Typography, useTheme } from '@mui/material'
+import Accordion from '@mui/material/Accordion'
+import AccordionSummary from '@mui/material/AccordionSummary'
+import AccordionDetails from '@mui/material/AccordionDetails'
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore'
 
-const FAQ = ()=>{
+import Header from '../../components/Header'
+import { tokens } from '../../theme'
+
+
+const FAQ = () => {
+
+  const theme = useTheme()
+  const colors = tokens( theme.palette.mode )
+
   return (
-    <div>FAQ</div>
+    <Box m='20px'>
+      <Header title='FAQ' subtitle='Frequently Asked Questions' />
+
+      <Accordion defaultExpanded>
+        <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+          <Typography color={colors.greenAccent[ 500 ]} variant='h5'>
+            Question 1
+          </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          answer 1
+        </AccordionDetails>
+      </Accordion>
+
+
+      <Accordion defaultExpanded>
+        <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+          <Typography color={colors.greenAccent[ 500 ]} variant='h5'>
+            Question 2
+          </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          answer 2
+        </AccordionDetails>
+      </Accordion>
+    </Box>
   )
 }
 
